4e527bf81f9091d1815c2871e36fa912a5bd3112
micei1 <122587845+micei1@users.noreply.github.com>
2025-07-20 00:29:06 -0900

    Update logo.tsx

--- app/components/ui/logo.tsx
+++ app/components/ui/logo.tsx
@@ -8,9 +8,7 @@ export function Logo() {
       href="/"
       className="flex items-center gap-2 hover:opacity-80 transition-opacity"
     >
-      <div className="relative w-8 h-8">
           <img width=32 height=32 src='/icons/icon-192x192.png'>
-      </div>
       <span className="font-bold tracking-wider bg-clip-text text-transparent bg-gradient-to-r from-primary to-purple-600">
         VCUS
       </span>
